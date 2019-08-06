7a38a76851f7a595c2e7e74ea00627e959649efa
Branson Starr <bwstarr@uchicago.edu>
2019-08-06 16:46:12 -0500

    These are test edits for the anagrams function

--- tests.py
+++ tests.py
@@ -5,8 +5,19 @@ def main():
 		for word in file:
 			if (len(word) >= 8):
 				letters = word
+				letters = letters.lower()
 				key = "".join(sorted(letters))
-				print(key)
-				break
+				if key in anagrams:
+					anagrams[key].append(word)
+				else:
+					new_list = []
+					new_list.append(word)
+					d = {key : new_list}
+					anagrams.update(d)
+
+		print(anagrams)
+		
+
+				
 
 main()
