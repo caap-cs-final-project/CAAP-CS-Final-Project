a452fa5fbdeb16b4ad1b95262375764e6684fb86
Sophia Pegues <sophia11@uchicago>
2019-08-06 17:22:13 -0500

    Modifying Anagrams code to print anagrams corrrectly

--- tests.py
+++ tests.py
@@ -1,21 +1,24 @@
 
 def main():
-	with open("eng_dict.txt", "r") as file:
-		anagrams = {}
-		for word in file:
-			if (len(word) >= 8):
-				word = word.strip("\n")
-				letters = word.lower()
-				key = "".join(sorted(letters))
-				if key in anagrams:
-					anagrams[key].append(word)
-				else:
-					new_list = []
-					new_list.append(word)
-					d = {key : new_list}
-					anagrams.update(d)
-
-		print(anagrams)
+    with open("eng_dict.txt", "r") as file:
+        words = file.read().splitlines()
+        anagrams = {}
+        for word in words:
+            if (len(word) >= 8):
+                #word = word.strip("\n")
+                letters = word.lower()
+                key = "".join(sorted(letters))
+                if key in anagrams:
+                    anagrams[key].append(word)
+                else:
+                    #new_list = []
+                    #new_list.append(word)
+                    #d = {key : new_list}
+                    anagrams[key] = [word]
+    print(anagrams)
+    for key in anagrams:
+        if len(anagrams[key]) > 1:
+            print("Found some anagrams")
 		
 
 				
